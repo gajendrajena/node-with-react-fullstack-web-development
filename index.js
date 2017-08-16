f08616d54ffde5835315bca07da7d653b8a7a2b3
Gajendra Jena <gjena@ctepl.com>
2017-08-16 18:46:50 +0530

    oauth phase1

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ passport.use(
         console.log('accessToken: ' + accessToken);
         console.log('refreshToken: ' + refreshToken);
         console.log('profile: ' + profile);
-        console.log('done: ' + done);
       }
       )
     );
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email']}));
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email']}));
+
 app.get('/auth/google/callback', passport.authenticate('google'));
 
 
---
+++ readme.md
@@ -0,0 +1,1 @@
+http://localhost:5000/auth/google/callback?code=4/KDrtrdvkI47imzh270CJSqkLkFZAFmxfBWx0LJMi-SI#
