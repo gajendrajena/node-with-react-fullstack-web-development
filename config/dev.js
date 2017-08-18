b3063e7acaada30de77f6e2b35c3f58234806ac5
Gajendra Jena <gjena@ctepl.com>
2017-08-18 13:50:24 +0530

    finished auth flow and making it prod ready

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '336869593031-6g32qhbce6o5pumfss99qs5oktk7taff.apps.googleusercontent.com',
+  googleClientSecret: 'HNVWGhlAFXY75eu6V0RKBpkp',
+  mongoURI: 'mongodb://gajendrajena:okkkkkkkk890@ds145223.mlab.com:45223/mail-app-dev',
+  cookieKey: 'kjhhkjhkljhlkjjlklkjhjkjlkkjllkulkjjkjklkjhkljhjkllkjhkjhiuyiyiuy'
+};
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 http://localhost:5000/auth/google/callback?code=4/KDrtrdvkI47imzh270CJSqkLkFZAFmxfBWx0LJMi-SI#
+
+
+541129469004-unqmfbec45qs711d3tp0lb31e4ib3g8o.apps.googleusercontent.com
+vd6j5iMhsft6jOajr1mk_kom
+
+mongodb://gajendrajena:okkkkkkkk890@ds145293.mlab.com:45293/mail-app-prod  
